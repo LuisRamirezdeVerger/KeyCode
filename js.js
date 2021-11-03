2950ad23b60f0a711c8f57aadfe1732a09a0df54
Luis RdV <job.wito.uk@gmail.com>
2021-11-03 10:09:40 +0000

    KeyCode

--- js.js
+++ js.js
@@ -1,20 +1,29 @@
-/* In this example, we use a cross-browser solution, because the keyCode property does not work on the onkeypress event in Firefox. However, the which property does.
+const container = document.getElementById("container");
+document.addEventListener("keypress",(e)=>{
+  
+    let key = e.key;
+    let location = e.location;
+    let which = e.which;
+    let code = e.code;
+    document.getElementById("eventKey").innerHTML = key;
+    document.getElementById("keycode").innerHTML = which;
+    document.getElementById("eventLocation").innerHTML=location;
+    document.getElementById("eventWhich").innerHTML= which;
+    document.getElementById("eventCode").innerHTML=code;
+  
+});
 
-Explanation of the first line in the function below: if the browser supports event.which, then use event.which, otherwise use event.keyCode */
 
-// function myFunction(event) {
-//   let x = event.which || event.keyCode;
-//   document.getElementById("demo").innerHTML = "The Unicode value is: " + x;
-// }
+// document.addEventListener("keypress", (e) => {
 
-// window.addEventListener("keydown", function(event) {
-//     const p = document.createElement("p");
-//     p.textContent = `KeyboardEvent: key='${event.key}' | code='${event.code}'`;
-//     document.getElementById("output").appendChild(p);
-//   }, true);
+//   let key = e.key;
+//   let location = e.location;
+//   let which = e.which;
+//   let code = e.code;
+//   document.getElementById("eventKey").innerHTML = key;
+//   document.getElementById("keycode").innerHTML = which;
+//   document.getElementById("eventLocation").innerHTML = location;
+//   document.getElementById("eventWhich").innerHTML = which;
+//   document.getElementById("eventCode").innerHTML = code;
 
-  window.addEventListener("keydown", function(e){
-      const p = this.document.createElement("p");
-      p.textContent = `KeyboardEvent: key='${e.key}' | code = '${e.code}'`;
-      document.getElementById("output").appendChild(p);
-  }, true);
+// });
